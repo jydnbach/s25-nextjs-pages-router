fdd1714aed9bf9803e6cd46e7ce599dc10b9464b
jydnbach <jaydenkim95@gmail.com>
2024-06-27 17:17:32 +0900

    extracting newsid using router

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,13 @@
+import { useRouter } from 'next/router';
+
 //  main-domain.com/news/detail
 
 function DetailPage() {
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+  console.log(newsId);
+
   return <h1>DetailPage</h1>;
 }
 export default DetailPage;
