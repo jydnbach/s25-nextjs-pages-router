6fc23041eb418f9c456ec1049475842cdd0cd720
jydnbach <jaydenkim95@gmail.com>
2024-06-27 17:27:48 +0900

    Link instead of a

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,6 +1,17 @@
+import Link from 'next/link';
 //  main-domain.com/news
 
 function NewsPage() {
-  return <h1>News</h1>;
+  return (
+    <>
+      <h1>News</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs">nextjs</Link>
+        </li>
+        <li>nextjs 2</li>
+      </ul>
+    </>
+  );
 }
 export default NewsPage;
